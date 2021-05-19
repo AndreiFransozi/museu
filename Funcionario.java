c1b0c2f445b795d1f748d11a93c7581f8663376e
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2021-05-19 14:27:08 -0300

    Add files via upload

---
+++ Funcionario.java
@@ -0,0 +1,34 @@
+
+package prjcontroladormuseu;
+public class Funcionario {
+  private String nome; 
+  private String fone;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the fone
+     */
+    public String getFone() {
+        return fone;
+    }
+
+    /**
+     * @param fone the fone to set
+     */
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+}
---
+++ Guarda.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjcontroladormuseu;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class Guarda {
+private time horaEntrada;   
+private time horaSaida;
+private turno;
+}
